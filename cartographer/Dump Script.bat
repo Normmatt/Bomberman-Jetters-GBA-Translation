d7155c9016f344a495f9bcef3c284de15e843d55
Normmatt <normmatt234@gmail.com>
2011-01-19 16:29:20 +1300

    update script dumping tools.

--- cartographer/Dump Script.bat
+++ cartographer/Dump Script.bat
@@ -1,3 +1,4 @@
-cartographer input.gba bomberman_commands.txt bomberman_script -m
+cartographer ../rom/input.gba bomberman_commands.txt bomberman_script -s
+cartographer ../rom/input.gba bomberman_commands.txt bomberman_script -m
 
 pause
