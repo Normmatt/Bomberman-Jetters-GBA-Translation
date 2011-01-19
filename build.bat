d976e3d11e397480ed96402f91c6b3ae6e3e4eed
Normmatt <normmatt234@gmail.com>
2011-01-19 20:17:11 +1300

    opps forgot commit this file.

--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
-cp input.gba output.gba
-armips.exe SmallVWF.asm
-Atlas output.gba bomberman_script_000.txt
-Atlas output.gba bomberman_script_001.txt
+cp rom/input.gba rom/output.gba
+armips.exe asm/SmallVWF.asm
+Atlas rom/output.gba script/bomberman_script_000.txt
+Atlas rom/output.gba script/bomberman_script_001.txt
+Atlas rom/output.gba script/bomberman_script_002.txt
+Atlas rom/output.gba script/bomberman_script_003.txt
+Atlas rom/output.gba script/bomberman_script_004.txt
+Atlas rom/output.gba script/bomberman_script_005.txt
+Atlas rom/output.gba script/bomberman_script_006.txt
+Atlas rom/output.gba script/bomberman_script_007.txt
+Atlas rom/output.gba script/bomberman_script_008.txt
+Atlas rom/output.gba script/bomberman_script_009.txt
 pause
