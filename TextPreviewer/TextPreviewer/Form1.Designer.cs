688af7fb88c500b4c40a544a32f608173ccf6695
Normmatt <normmatt234@gmail.com>
2011-05-25 17:19:36 +1200

    Commit text previewer

---
+++ TextPreviewer/TextPreviewer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TextPreviewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(618, 217);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "The Quick Brown Fox Jumped Over The Lazy Dog.";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(232)))), ((int)(((byte)(184)))));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 236);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Previous Box";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(554, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "&Next Box";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(243, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "&Auto Format";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 394);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Bomberman Jetters Text Previewer and Formatter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TextPreviewer/TextPreviewer/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TextPreviewer
+{
+    public partial class Form1 : Form
+    {
+        const string appName = "TextPreviewer";
+        int cur_x = 0;
+        int cur_y = 4;
+        Image fontImage = null;
+        Image textboxImage = null;
+        Bitmap finalImg = null;
+
+        byte[] WidthFile;
+        Dictionary<string, string> table;
+        Encoding encoding = Encoding.GetEncoding(932); //SJIS
+
+        int numNewLines = 0;
+        int curLine = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int GetCharacter(char character)
+        {
+            /*for (int i = 0; i < table.Keys.Count; i++)
+            {
+                string j = i.ToString("X").PadLeft(2, '0');
+                if (table[j] == character.ToString())
+                {
+                    return i;
+                }
+            }*/
+
+            try
+            {
+                return Convert.ToInt32(table[character.ToString()],16);
+            }
+            catch
+            {
+                //System.Diagnostics.Trace.WriteLine("Byte not found in table: " + j);
+                //tmp += "{" + j + "}";
+                MessageBox.Show("Byte not found in table: " + character, appName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                //return "Error";
+            }
+            return 0;
+        }
+
+        private int GetCharacterWidth(int character)
+        {
+            return WidthFile[character];
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            //a holder for the result
+            Bitmap result = new Bitmap(width, height);
+
+            //use a graphics object to draw the resized image into the bitmap
+            using (Graphics graphics = Graphics.FromImage(result))
+            {
+                //set the resize quality modes to high quality
+                graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+                //draw the image into the target bitmap
+                graphics.DrawImage(image, 0, 0, result.Width, result.Height);
+            }
+
+            //return the resulting bitmap
+            return result;
+        }
+
+        private void DrawText()
+        {
+            string text = "";
+
+            if (textBox1.Lines.Length > curLine)
+            {
+                text += textBox1.Lines[curLine];
+            }
+
+            if (textBox1.Lines.Length > curLine + 1)
+            {
+                text += Environment.NewLine;
+                text += textBox1.Lines[curLine + 1];
+            }
+
+            if (textBox1.Lines.Length > (curLine + 2))
+            {
+                button2.Enabled = true;
+            }
+
+            GraphicsUnit units = GraphicsUnit.Pixel;
+            // Draw image to screen.
+            Graphics gfx = Graphics.FromImage(finalImg);
+
+            //Textbox stuff
+            Rectangle tdRect = new Rectangle(0, 0, 206, 40);
+            // Create rectangle for source image.
+            Rectangle tsRect = new Rectangle(0, 0, 206, 40);
+            gfx.DrawImage(textboxImage, tdRect, tsRect, units);
+
+            cur_x = 15;
+            cur_y = 8;
+            text = text.Replace("<LINE>", "");
+            text = text.Replace("<END>", "");
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '\r' && text[i+1] == '\n') //New Line
+                {
+                    cur_x = 15;
+                    cur_y += 16;
+                    i += 2;
+
+                    if (i >= text.Length) return;
+                }
+
+                if (cur_x < 191)
+                {
+                    int cur_char = GetCharacter(text[i]);
+                    int cur_width = GetCharacterWidth(cur_char);
+                    int src_x = (cur_char % 0x10) * 8;
+                    int src_y = (cur_char / 0x10) * 8;
+                    // Create rectangle for displaying image.
+                    Rectangle destRect = new Rectangle(cur_x, cur_y, cur_width, 8);
+                    // Create rectangle for source image.
+                    Rectangle srcRect = new Rectangle(src_x, src_y, cur_width, 8);
+
+                    gfx.DrawImage(fontImage, destRect, srcRect, units);
+
+                    cur_x += cur_width;
+
+                }
+                //else
+                //    MessageBox.Show("The line is too long.");
+            }
+
+            pictureBox1.Image = ResizeImage(finalImg, finalImg.Width * 3, finalImg.Height * 3);
+        }
+
+        /// <summary>
+        /// Function to get byte array from a file
+        /// </summary>
+        /// <param name="_FileName">File name to get byte array</param>
+        /// <returns>Byte Array</returns>
+        public byte[] FileToByteArray(string _FileName)
+        {
+            byte[] _Buffer = null;
+
+            try
+            {
+                // Open file for reading
+                System.IO.FileStream _FileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+
+                // attach filestream to binary reader
+                System.IO.BinaryReader _BinaryReader = new System.IO.BinaryReader(_FileStream);
+
+                // get total byte length of the file
+                long _TotalBytes = new System.IO.FileInfo(_FileName).Length;
+
+                // read entire file into buffer
+                _Buffer = _BinaryReader.ReadBytes((Int32)_TotalBytes);
+
+                // close file reader
+                _FileStream.Close();
+                _FileStream.Dispose();
+                _BinaryReader.Close();
+            }
+            catch (Exception _Exception)
+            {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}", _Exception.ToString());
+            }
+
+            return _Buffer;
+        }
+
+
+        bool loadTable(string filename)
+        {
+            try
+            {
+                FileStream fs = new FileStream(filename, FileMode.Open);
+                StreamReader sr = new StreamReader(fs, encoding);
+                string[] lines = sr.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                table = new Dictionary<string, string>();
+                int cur_line = 0;
+                foreach (string str in lines)
+                {
+                    if (str == "") break;
+
+                    int i = str.IndexOf("=");
+                    string sByte;
+                    string sVal;
+                    if (i >= 0)
+                    {
+                        sByte = str.Substring(0, i).ToUpper();
+                        sVal = str.Substring(i + 1);
+                        //System.Diagnostics.Trace.WriteLine(sVal);
+                        table.Add(sVal, sByte);
+                        cur_line++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid table file.", appName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        fs.Dispose();
+                        return false;
+                    }
+                }
+                fs.Dispose();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Error accessing file.", appName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            /*numNewLines = 0;
+            int firstIndex = 0;
+            while(true)
+            {
+                int nextIndex = textBox1.Text.IndexOf(Environment.NewLine,firstIndex);
+                if (nextIndex == -1) break;
+
+                firstIndex = nextIndex+1;
+                numNewLines++;
+            }*/
+            DrawText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fontImage = Image.FromFile("font.png");
+            textboxImage = Image.FromFile("textbox.png");
+            loadTable("melissa8x8.tbl");
+            WidthFile = FileToByteArray("smallWidthTable.bin");
+            finalImg = new Bitmap(206, 40);
+
+            DrawText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            curLine -= 2;
+            if (curLine < 0)
+            {
+                curLine = 0;
+            }
+            DrawText();
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            curLine += 2;
+            if (curLine >= textBox1.Lines.Length)
+            {
+                curLine = textBox1.Lines.Length - 2;
+            }
+            DrawText();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            cur_x = 15;
+            cur_y = 8;
+            text = text.Replace(Environment.NewLine, " ");
+            text = text.Replace("<LINE>", "");
+            text = text.Replace("<END>", "");
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '\r' && text[i + 1] == '\n') //New Line
+                {
+                    cur_x = 15;
+                    cur_y += 16;
+                    i += 2;
+
+                    if (i >= text.Length) return;
+                }
+
+                cur_x += GetCharacterWidth(GetCharacter(text[i]));
+
+                if (cur_x > 191)
+                {
+                    if (i > text.Length) i = text.Length - 1;
+                    int lastSpace = text.LastIndexOf(" ", i);
+                    text = text.Remove(lastSpace, 1);
+                    textBox1.Text = text.Insert(lastSpace, Environment.NewLine);
+                    return;
+                    //MessageBox.Show("The line is too long.");
+                    //return;
+                }
+            }
+        }
+
+        
+    }
+}
