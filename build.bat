ce070e699a26128ac2670b84a7b7ed5e4610428e
Normmatt <normmatt234@gmail.com>
2012-02-07 15:11:14 +1300

    Make it patch in the new asm hacks.

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 copy rom\input.gba rom\output.gba
 armips.exe asm/SmallVWF.asm
+armips.exe asm/Cheats.asm
+armips.exe asm/DebugMenu.asm
+armips.exe asm/graphicsChanges.asm
+armips.exe asm/ItemNamesVWF.asm
 Atlas rom\output.gba script\bomberman_script_000.txt
 Atlas rom\output.gba script\bomberman_script_001.txt
 Atlas rom\output.gba script\bomberman_script_002.txt
@@ -10,4 +14,5 @@ Atlas rom\output.gba script\bomberman_script_006.txt
 Atlas rom\output.gba script\bomberman_script_007.txt
 Atlas rom\output.gba script\bomberman_script_008.txt
 Atlas rom\output.gba script\bomberman_script_009.txt
+copy rom\output.gba rom\Bomberman_Jetters(E).gba
 pause
